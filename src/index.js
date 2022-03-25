1df042f0af2f341abff158dccbdb056beab23290
Juan David De la hoz Montero <juan.delahoz@silvercredit.co>
2022-03-25 16:48:21 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {Server as WebSocketServer } from 'socket.io';
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+const io = new WebSocketServer(server, { 
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('nueva conexion:', socket.id);
+})
+
+server.listen(4000);
+console.log('Server on port', 4000);
